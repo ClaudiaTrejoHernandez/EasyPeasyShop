a7c5ae548787a86dcf3cedd11919f668881f7127
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-24 11:17:07 -0700

    Began working on methods
    -Got the categories up and running

--- Capstone3-main/src/main/java/org/yearup/controllers/CategoriesController.java
+++ Capstone3-main/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,39 +9,51 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
+
+
 // add the annotations to make this a REST controller
+@RestController
+
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
-public class CategoriesController
-{
+@RequestMapping("/categories")
+@CrossOrigin
+public class CategoriesController {
+
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
+// create an Autowired controller to inject the CategoryDao and ProductDao
+    @Autowired
+    CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
 
-    // add the appropriate annotation for a get action
+// add the appropriate annotation for a get action
+    @GetMapping("")
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
+// add the appropriate annotation for a get action
+    @GetMapping("/{categoryId}")
+    public Category getById(@PathVariable int categoryId) {
+
         // get the category by id
-        return null;
+        return categoryDao.getById(categoryId);
     }
 
-    // the url to return all products in category 1 would look like this
+// the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return null; //productDao.getById(categoryId);
     }
 
     // add annotation to call this method for a POST action
