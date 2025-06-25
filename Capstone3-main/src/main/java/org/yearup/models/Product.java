d909b6bcaff67005c67be09965724aa96015ecea
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-25 11:48:37 -0700

    Erased spaces

--- Capstone3-main/src/main/java/org/yearup/models/Product.java
+++ Capstone3-main/src/main/java/org/yearup/models/Product.java
@@ -14,9 +14,7 @@ public class Product
     private boolean isFeatured;
     private String imageUrl;
 
-    public Product()
-    {
-    }
+    public Product() {}
 
     public Product(int productId, String name, BigDecimal price, int categoryId, String description, String color, int stock, boolean isFeatured, String imageUrl)
     {
