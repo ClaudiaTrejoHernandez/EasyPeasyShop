fe61cb5f2a3cbe23f95b094ce4250637b6c5fb44
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-27 08:07:04 -0700

    Added method signatures

--- Capstone3-main/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ Capstone3-main/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,8 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    ShoppingCart addProductToCart(int userId, int productId);
+    ShoppingCart removeProductFromCart(int userId, int productId);
+    ShoppingCart updateQuantity(int userId, int productId, int quantity);
+
 }
