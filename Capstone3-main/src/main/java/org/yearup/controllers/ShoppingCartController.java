406355e0d083e58d54be968050cdeee46143a9c3
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-27 08:05:50 -0700

    Adding logic to the ShoppingCartController methods that were previously empty
    -May continue to update and change

--- Capstone3-main/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ Capstone3-main/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -11,21 +13,29 @@ import org.yearup.models.User;
 import java.security.Principal;
 
 // convert this class to a REST controller
+@RestController
+@CrossOrigin
 // only logged in users should have access to these actions
-public class ShoppingCartController
-{
+@RequestMapping("/cart")
+public class ShoppingCartController {
+
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
 
-
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
+
+    @GetMapping("")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ShoppingCart getCart(Principal principal) {
+        try {
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
@@ -33,20 +43,59 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
-        }
-        catch(Exception e)
-        {
+            return shoppingCartDao.getByUserId(userId);
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
-    // add a POST method to add a product to the cart - the url should be
+// add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
 
+    @PostMapping("/{productId}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addToCart(@PathVariable int productId, Principal principal) {
 
-    // add a PUT method to update an existing product in the cart - the url should be
+        try{
+            //Get currently logged in user
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            //Add product to the cart using the ShoppingCartDao
+            shoppingCartDao.addProductToCart(userId, productId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to add to cart.");
+        }
+    }
+
+
+// add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
+
+    @PutMapping("/{productId}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void updateCartItemQuantity(@PathVariable int productId,@RequestBody int quantity, Principal principal) {
+
+        try{
+            //Get currently logged in user
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            //Add product to the cart using the ShoppingCartDao
+            if (quantity <= 0) {
+                shoppingCartDao.removeProductFromCart(userId, productId);
+            } else {
+                shoppingCartDao.updateQuantity(userId, productId, quantity);
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to update cart item.");
+        }
+    }
+
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
 
 
